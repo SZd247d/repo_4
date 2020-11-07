98ef5f98a1d1a4d560dc09b43fda3f6050f6b5fb
zakaria sellali <zakisellali464@gmail.com>
2020-11-07 09:35:09 +0100

    the BACK_END with node, express & mongoose for first_repo

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+dotenv.config({ path: './config/config.env' });
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/api/v1/transactions', require('./routes/transactions'));
+
+// I'm not sure that the Path is correct
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (req, res) =>
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
